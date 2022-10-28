7feb1cd6c6c1a7da28c12e660c751c8e669950da
Нурлан <@->
2022-10-28 21:56:14 +0400

    Добавьте файлы проекта.

---
+++ IMT9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IMT9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Ros, Ves, IMT;
+            Ros = 0;
+            Ves = 0;
+            try
+            {
+                Console.WriteLine("Введите рост");
+                Ros = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите вес");
+                Ves = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Нужно ввести число");
+                Console.ReadKey();
+            }
+            IMT = Ves / Ros;
+            Console.WriteLine(IMT);
+            Console.ReadKey();
+        }
+    }
+}
